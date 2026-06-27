a9a7df83958e475d6ee158daf43ede8bb14f0dd1
rgeshev <Rosen_Geshev19@abv.bg>
2026-06-27 13:23:17 +0300

    Set up dev enviroment

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!!!!!');
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
